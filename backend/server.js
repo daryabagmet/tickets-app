e9a065be29e641d040d97ee376b28e62471f5e27
darya <darya@MBP-darya.Home>
2022-12-24 17:31:13 +0000

    add static path

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -12,14 +13,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.status(200).json({
-		message: 'Welcome to the Support Desk API',
-	});
-});
-
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*'), (req, res) => res.sendFile(__dirname, '../', 'fronted', 'buid', 'index.html')
+}else {
+	app.get('/', (req, res) => {
+		res.status(200).json({
+			message: 'Welcome to the Support Desk API',
+		});
+	});
+}
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
